0031410dd6ea739fbbb1e34c17a90eb6462208a1
Le Duy Thuc <leduykhongngu@gmail.com>
2020-04-13 14:46:15 +0700

    remove dot

--- cogs/Graph.py
+++ cogs/Graph.py
@@ -370,9 +370,9 @@ class Graph(commands.Cog):
         if current_badge.high < 100:
             upper_bound = current_badge.high * badge.MAX_SCORE / 100
             nxt_badge = badge.point2rank(upper_bound + 1, MAX_SCORE=badge.MAX_SCORE)
-            msg = "{} cần khoảng {:.2f} exp nữa để có badge {} {}.".format(
+            msg = "{} cần khoảng {:.2f} exp nữa để có badge {} {}".format(
                 handle, upper_bound, nxt_badge.title,
-                '<:megu_hi:699161403436695557>'
+                '<:megu_hi:699162801226252289>'
             )
         discord_file = gc.get_current_figure_as_file()
         embed = discord_common.cf_color_embed(
